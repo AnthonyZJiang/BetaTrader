c3a84683549004c3a25b1e4d9f965ffc5f93bb3d
Zhengyi Jiang <anthony.jiang.github@outlook.com>
2024-07-28 00:32:14 +0100

    fixed bugs

--- betatrader/trendanalyser.py
+++ betatrader/trendanalyser.py
@@ -54,73 +54,21 @@ class TrendAnalyser:
             self.klines[self.pointer].trend_since = self.pointer
         self.klines[self.pointer].trend_length = self.pointer - self.klines[self.pointer].trend_since + 1
         
-        self.check_signals()
-        
+        self._check_signals()
     
-    def calculate_micro_pullback_pattern(self) -> float:
-        """Returns likelihood of a micro pullback pattern"""
-        if self.klines[self.pointer].is_green:
-            return 0
-        if self.pointer < 4:
-            return 0
-        likelihood = 1
+    def _check_signals(self):
+        self._calculate_ema()
+        self._calculate_macd()
+        self._find_pattern()
         
-        down_trend_length = self.klines[self.pointer].trend_length
-        likelihood -= 0.3 * (down_trend_length - 1)
-        if likelihood < 0:
-            return 0
-        print(self.klines[-1].data['date'])
-        down_trend_start = self.klines[self.pointer].trend_since
-        up_trend_end = self.pointer - down_trend_length
-        up_trend_start = self.klines[up_trend_end].trend_since
-        
-        red_candles = self.klines[down_trend_start:self.pointer + 1]
-        red_max_volume = max([c.volume for c in red_candles])
-        red_min_close = min([c.close_price for c in red_candles])
-        green_candles = self.klines[up_trend_start:up_trend_end + 1]
-        green_max_volume = max([c.volume for c in green_candles])
-        green_max_open = max([c.open_price for c in green_candles])
-        green_last_close = self.klines[up_trend_end].close_price
-        up_amount = self.klines[up_trend_end].close_price - green_candles[0].open_price
-        drop_amount = green_last_close - red_min_close
-        
-        if green_max_volume == 0:
-            return 0
-        
-        if red_min_close < green_max_open or drop_amount/up_amount > 0.3:
-            return 0
-        
-        ratio = red_max_volume/green_max_volume
-        deduction = -0.01 - 0.003 * math.exp(5.87*ratio)
-        deduction = max(0, deduction)
-        likelihood += deduction
-        self.klines[self.pointer].micro_pullback_likelihood = likelihood
-        
-    def calculate_macd(self):
-        self.klines[self.pointer].macd_curve = self.klines[self.pointer].ema12 - self.klines[self.pointer].ema26
-        self.klines[self.pointer].macd_signal = ema(self.klines[self.pointer].macd_curve, self.klines[self.pointer-1].macd_signal, 9)
-        self.klines[self.pointer].macd = self.klines[self.pointer].macd_curve - self.klines[self.pointer].macd_signal
-        
-    def calculate_ema(self):
-        if self.pointer == 0:
-            return
-        close_price = self.klines[self.pointer].close_price
-        self.klines[self.pointer].ema9 = ema(close_price, self.klines[self.pointer-1].ema9, 9)
-        self.klines[self.pointer].ema12 = ema(close_price, self.klines[self.pointer-1].ema12, 12)
-        self.klines[self.pointer].ema20 = ema(close_price, self.klines[self.pointer-1].ema20, 20)
-        self.klines[self.pointer].ema26 = ema(close_price, self.klines[self.pointer-1].ema26, 26)
-    
-    def check_signals(self):
-        self.calculate_micro_pullback_pattern()
-        self.calculate_ema()
-        self.calculate_macd()
-        
+        # micro pullback entry signal
         if self.klines[self.pointer - 1].micro_pullback_likelihood and self.klines[self.pointer].is_green > 0.5:
             previous_high = max([i.open_price for i in self.klines[self.pointer-1:self.klines[self.pointer - 1].trend_length+1:-1]])
             if self.klines[self.pointer].high_price > previous_high:
                 self.klines[self.pointer].entry_signal = previous_high
-            return
+                return
         
+        # ema and macd signals
         gold_crossed = False
         death_crossed = False
         
@@ -158,4 +106,57 @@ class TrendAnalyser:
         elif death_crossed and not ema_good and not macd_good:
             self.klines[self.pointer].exit_signal = self.klines[self.pointer].close_price
         
+    def _find_pattern(self) -> float:
+        """Returns likelihood of a micro pullback pattern"""
+        if self.pointer < 4:
+            return
+        if self.klines[self.pointer].is_green:
+            return
         
+        self.dt_length = self.klines[self.pointer].trend_length
+        self.ut_end = self.pointer - self.dt_length
+        if self.ut_end <= 0:
+            return
+        
+        self.ut_start = self.klines[self.ut_end].trend_since
+        self.ut_candles = self.klines[self.ut_start:self.ut_end + 1]
+        self.ut_max_volume = max([c.volume for c in self.ut_candles])
+        if self.ut_max_volume == 0:
+            return
+        
+        self.dt_start = self.klines[self.pointer].trend_since
+        self.dt_candles = self.klines[self.dt_start:self.pointer + 1]
+        self.dt_max_volume = max([c.volume for c in self.dt_candles])
+        self.dt_min_close = min([c.close_price for c in self.dt_candles])
+        self.ut_max_open = max([c.open_price for c in self.ut_candles])
+        self.ut_last_close = self.klines[self.ut_end].close_price
+        self.ut_amount = self.klines[self.ut_end].close_price - self.ut_candles[0].open_price
+        self.drop_amount = self.ut_last_close - self.dt_min_close
+        
+        self._calculate_micro_pullback_pattern()
+        
+    def _calculate_micro_pullback_pattern(self):
+        likelihood = 1 - 0.3 * (self.dt_length - 1)
+        if likelihood < 0:
+            return
+        if self.dt_min_close < self.ut_max_open or self.drop_amount/self.ut_amount > 0.3:
+            return
+        ratio = self.dt_max_volume/self.ut_max_volume
+        deduction = -0.01 - 0.003 * math.exp(5.87*ratio)
+        deduction = max(0, deduction)
+        likelihood += deduction
+        self.klines[self.pointer].micro_pullback_likelihood = likelihood
+        
+    def _calculate_macd(self):
+        self.klines[self.pointer].macd_curve = self.klines[self.pointer].ema12 - self.klines[self.pointer].ema26
+        self.klines[self.pointer].macd_signal = ema(self.klines[self.pointer].macd_curve, self.klines[self.pointer-1].macd_signal, 9)
+        self.klines[self.pointer].macd = self.klines[self.pointer].macd_curve - self.klines[self.pointer].macd_signal
+        
+    def _calculate_ema(self):
+        if self.pointer == 0:
+            return
+        close_price = self.klines[self.pointer].close_price
+        self.klines[self.pointer].ema9 = ema(close_price, self.klines[self.pointer-1].ema9, 9)
+        self.klines[self.pointer].ema12 = ema(close_price, self.klines[self.pointer-1].ema12, 12)
+        self.klines[self.pointer].ema20 = ema(close_price, self.klines[self.pointer-1].ema20, 20)
+        self.klines[self.pointer].ema26 = ema(close_price, self.klines[self.pointer-1].ema26, 26)
