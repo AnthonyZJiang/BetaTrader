8488c3233b9c8c9212c461b9f0a0d3418e1251cd
Zhengyi Jiang <anthony.jiang.github@outlook.com>
2024-07-29 17:46:42 +0100

    added decorator for fmp, added scanner api

--- betatrader/fmp.py
+++ betatrader/fmp.py
@@ -1,5 +1,14 @@
 import requests
+import json
 
+def write_to_file(func):
+    def _call_wrapper(*args, **kwargs):
+        if filename := kwargs.pop('filename'):
+            response = func(*args, **kwargs)
+            with open(filename, 'w') as f:
+                json.dump(response, f, indent=4)
+        return response
+    return _call_wrapper
 
 class FMP:
     """Financial Modeling Prep API"""    
@@ -11,21 +20,44 @@ class FMP:
             'Content-Type': 'application/json'
         }
         
+    @write_to_file
     def get_historical_price_full(self, symbol):
         url = self.construct_url('historical-price-full', symbol)
         response = requests.get(url, headers=self.headers)
         return response.json()
     
+    @write_to_file
     def get_emas(self, timeframe: str, symbol: str, period: int):
         url = self.construct_url(['technical_indicator', timeframe, symbol],  ['type=ema', f'period={period}'])
         response = requests.get(url, headers=self.headers)
         return response.json()
     
+    @write_to_file
     def get_chart_intraday(self, symbol: str, interval: str, from_date: str, to_date: str, extended: bool = False):
         url = self.construct_url(['historical-chart', interval, symbol],  [f'from={from_date}', f'to={to_date}', f'extended={"true" if extended else "false"}'])
         response = requests.get(url, headers=self.headers)
         return response.json()
     
+    @write_to_file
+    def get_scanner(self, **kwargs):
+        """Find stocks that meet your investment criteria with our Screener (Stock) endpoint.
+
+        Args:
+            kwargs: available parameters are marketCapMoreThan,  marketCapLowerThan, priceMoreThan, priceLowerThan, betaMoreThan, betaLowerThan,
+            volumeMoreThan, volumeLowerThan, dividendMoreThan, dividendLowerThan, isEtf, isFund, isActivelyTrading, sector, industry, country, exchange, limit
+        """
+        if kwargs is None or len(kwargs) < 1:
+            return None
+        
+        params = []
+        for key, value in kwargs.items():
+            if value:
+                params.append(f'{key}={value}')
+        
+        url = self.construct_url(['stock-screener'],  params)
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+ 
     def construct_url(self, path_params: list | str, query_params: list | str) -> str:
         if isinstance(path_params, list):
             path_params = '/'.join(path_params)
--- datadownloader.py
+++ datadownloader.py
@@ -5,10 +5,15 @@ with open('credential.json', 'r') as f:
     cred = json.load(f)
 fmp = FMP(cred['api_key'])
 
-def get_intraday(symbol, interval, date_from, date_to, extended):
-    data = fmp.get_chart_intraday(symbol, interval, date_from, date_to, extended)
-    with open(f'data/{symbol}_{interval}_{date_from}{"_extended" if extended else ""}.json', 'w') as f:
-        json.dump(data, f, indent=4)
-    print("Data saved")
-        
-get_intraday('SLRX', '1min', '2024-07-23', '2024-07-23', True)
+
+# Scanner
+fmp.get_scanner(filename="scanner_result.json", priceMoreThan=1, priceLowerThan=20, limit=1000000, isActivelyTrading="true", isFund="false", isEtf="false")
+
+# Intraday
+symbol = "SLRX"
+interval = "1min"
+date_from = "2024-07-23"
+date_to = "2024-07-23"
+extended = True
+filename = f'data/{symbol}_{interval}_{date_from}{"extended" if extended else ""}.json'
+fmp.get_chart_intraday(symbol, interval, date_from, date_to, extended, filename=filename)
