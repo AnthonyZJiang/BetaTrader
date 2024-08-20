294df1599933f809ecd195acfeb92916b2e515d5
Anthony Jiang <anthony.zyjiang@outlook.com>
2024-08-21 00:20:29 +0100

    improved simulator for faster order placing

--- ikbr_simulator/simulator.py
+++ ikbr_simulator/simulator.py
@@ -33,7 +33,7 @@ class SimulatorCLI:
         self.print_tracking_symbol = False
         while not exit_flag:
             try:
-                exit_flag = not self._process_command()
+                exit_flag = not self._take_command()
             except Exception as e:
                 print("Invalid argument")
                 self.logger.error(e)
@@ -66,8 +66,13 @@ class SimulatorCLI:
         
     def _gui_update_callback_tracked_symbol(self, symbol):
         print(f"Tracking symbol: {symbol}")
-    
-    def _process_command(self):
+
+    def _place_order(self, order: Order):
+        if order.symbol is None:
+            return
+        self.tws_app.place_order(order)
+
+    def _take_command(self):
         if self.print_tracking_symbol:
             print("Tracking symbol:", self.tws_app.tws_common.current_symbol)
         self.print_tracking_symbol = True
@@ -83,121 +88,126 @@ class SimulatorCLI:
             print("Exiting")
             self.tws_app.disconnect()
             return False
+        return self._process_command(val.split())
         
-        args = val.split()
-        narg = len(args)
+    def _process_command(self, args: dict[str]):
+        nargs = len(args)
+
+        match args[0].lower():
+            case "help":
+                self._print_help()
+            case "set":
+                self._set_parameters(*args[1:])
+            case "ls":
+                if nargs == 1:
+                    self.tws_app.tws_common.portfolio.print_all_trades()
+                elif args[1] == "pos":
+                    self.tws_app.tws_common.portfolio.print_positions()
+            case "track":
+                self._set_track(*args[1:])
+            case "t":
+                self._set_track(*args[1:])
+            case "link":
+                if nargs == 1:
+                    print("No value provided")
+                    return True
+                self.print_tracking_symbol = False
+                id = int(args[1])
+                self.tws_app.link_display_group(id)
+            case "b":
+                order = self._get_order(OrderAction.BUY, *args[1:])
+                self._place_order(order)
+            case "s":
+                order = self._get_order(OrderAction.SELL, *args[1:])
+                self._place_order(order)
+            case "cl":
+                pos = self.tws_app.tws_common.portfolio.get_position(self.tws_app.tws_common.current_symbol)
+                if pos is None or pos.quantity <= 0:
+                    print("No position to close")
+                    return True
+                order = Order(self.tws_app.tws_common.current_symbol, OrderAction.SELL, pos.quantity)
+                self._place_order(order)
+            case "c":
+                if nargs == 1:
+                    print("No value provided")
+                    return True
+                order_id = int(args[1])
+                self.tws_app.cancel_order(order_id)
+            case "export":
+                if nargs == 1:
+                    dest = "trades.csv"
+                else:
+                    dest = args[1]
+                self.tws_app.tws_common.portfolio.export_trades(dest)
+            case _:
+                if args[0].startswith("s") or args[0].startswith("b"):
+                    return self._process_command([args[0][0], args[0][1:]] + args[1:])
+                else:
+                    print("Invalid command")
+        return True
+    
+    def _set_track(self, *args):
+        if len(args) == 0:
+            print("No value provided")
+            return
+        symbol = args[0].upper()
+        self.tws_app.update_linked_group_by_symbol(symbol)
+
+    def _get_order(self, action: OrderAction, *args):
         quantity = self.default_quantity
         limit = None
         stop = None
-        if args[0] == "help":
-            console = Console()
-            console.rule("Commands")
-            console.print("ls [green]Print all orders")
-            console.print("ls pos [green]Print all orders or positions")
-            console.print("set <default_quantity <quantity>> [green]Set the default quantity, default is 100")
-            console.print("set <allow_short <1|0>> [green]Set 1 to allow short selling")
-            console.print("track <symbol> [green]Track a symbol")
-            console.print("b <q<quantity>> <l<limit>> [green]Buy the tracked symbol")
-            console.print("s <q<quantity>> <l<limit>> [green]Sell the tracked symbol")
-            console.print("st <q<quantity>> <l<limit>> [green]Set stop loss for the tracked symbol")
-            console.print("cl [green]Close the current position for the tracked symbol")
-            console.print("c <order_id> [green]Cancel an order")
-            console.print("exit [green]Exit the program")
-            return True
-        if args[0] == "ls":
-            if narg == 1:
-                self.tws_app.tws_common.portfolio.print_all_trades()
-            elif args[1] == "pos":
-                self.tws_app.tws_common.portfolio.print_positions()
-            return True
-                      
-        if args[0] == "set":
-            if narg == 1:
-                return True
-            if args[1] == "default_quantity":
-                if narg == 2:
-                    print("No value provided")
-                    return True
-                self.default_quantity = int(args[2])
-            elif args[1] == "allow_short":
-                if narg == 2:
-                    print("No value provided")
-                    return True
-                self.allow_short = True if args[2] == "1" else False
-            elif args[1] == "quantity_multiplier":
-                if narg == 2:
-                    print("No value provided")
-                    return True
-                self.quantity_multiplier = int(args[2])
-            self.default_quantity = int(args[1])
-            return True
-            
-        if args[0] == "track":
-            if narg == 1:
-                print("No value provided")
-                return True
-            self.print_tracking_symbol = False
-            symbol = args[1].upper()
-            self.tws_app.update_linked_group_by_symbol(symbol)
-            
-        elif args[0] == "link":
-            if narg == 1:
-                print("No value provided")
-                return True
-            self.print_tracking_symbol = False
-            id = int(args[1])
-            self.tws_app.link_display_group(id)
-
-        elif args[0] == "b":
-            for arg in args[1:]:
-                if arg.startswith("q"):
-                    quantity = int(arg[1:])*self.quantity_multiplier
-                elif arg.startswith("st"):
-                    stop = float(arg[2:])
-                else:
-                    limit = float(arg)
-            order = Order(self.tws_app.tws_common.current_symbol, OrderAction.BUY, quantity, limit, stop)
-            self._place_order(order)
-        
-        elif args[0] == "s":
-            for arg in args[1:]:
-                if arg.startswith("q"):
-                    quantity = int(arg[1:])*self.quantity_multiplier
-                elif arg.startswith("st"):
-                    stop = float(arg[2:])
-                else:
-                    limit = float(arg)
-            order = Order(self.tws_app.tws_common.current_symbol, OrderAction.SELL, quantity, limit, stop)
-            self._place_order(order)
-            
-        elif args[0] == "close":
-            pos = self.tws_app.tws_common.portfolio.get_position(self.tws_app.tws_common.current_symbol)
-            if pos is None or pos.quantity <= 0:
-                print("No position to close")
-                return True
-            order = Order(self.tws_app.tws_common.current_symbol, OrderAction.SELL, pos.quantity)
-            self._place_order(order)
-            
-        elif args[0] == "c":
-            if narg == 1:
-                print("No value provided")
-                return True
-            order_id = int(args[1])
-            self.tws_app.cancel_order(order_id)
-            
-        elif args[0] == "export":
-            if narg == 1:
-                dest = "trades.csv"
+        i = 0
+        while i < len(args):
+            if args[i] == "l":
+                limit = float(args[i+1])
+                i += 2
+            elif args[i] == "st":
+                stop = float(args[i+1])
+                i += 2
             else:
-                dest = args[1]
-            self.tws_app.tws_common.portfolio.export_trades(dest)
-            
-        else:
-            print("Invalid command")
-        return True
+                quantity = int(args[i])*self.quantity_multiplier
+                i += 1
+        return Order(self.tws_app.tws_common.current_symbol, action, quantity, limit, stop)
 
-
-    def _place_order(self, order: Order):
-        if order.symbol is None:
+    def _set_parameters(self, *args):
+        nargs = len(args)
+        if nargs == 0:
             return
-        self.tws_app.place_order(order)
+        match args[0]:
+            case "default_quantity":
+                if nargs == 1:
+                    print("No value provided")
+                    return
+                self.default_quantity = int(args[1])
+                print("Default quantity set to", self.default_quantity)
+            case "allow_short":
+                if nargs == 1:
+                    print("No value provided")
+                    return
+                self.allow_short = True if args[1] == "1" else False
+                print("Short selling is", "enabled" if self.allow_short else "disabled")
+            case "quantity_multiplier":
+                if nargs == 1:
+                    print("No value provided")
+                    return
+                self.quantity_multiplier = int(args[1])
+                print("Quantity multiplier set to", self.quantity_multiplier)
+            case _:
+                print("Unknown parameter")
+        self.default_quantity = int(args[1])
+
+    def _print_help(self):
+        console = Console()
+        console.rule("Commands")
+        console.print("ls [green]Print all orders")
+        console.print("ls pos [green]Print all orders or positions")
+        console.print("set <default_quantity <quantity>> [green]Set the default quantity, default is 100")
+        console.print("set <allow_short <1|0>> [green]Set 1 to allow short selling")
+        console.print("track <symbol> [green]Track a symbol")
+        console.print("b <q<quantity>> <l<limit>> [green]Buy the tracked symbol")
+        console.print("s <q<quantity>> <l<limit>> [green]Sell the tracked symbol")
+        console.print("st <q<quantity>> <l<limit>> [green]Set stop loss for the tracked symbol")
+        console.print("cl [green]Close the current position for the tracked symbol")
+        console.print("c <order_id> [green]Cancel an order")
+        console.print("exit [green]Exit the program")
